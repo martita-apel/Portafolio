84db200dd11e85e4465a6be853844ef0dd092089
Martita Apel <mtapel@uc.cl>
2020-01-25 13:28:46 -0300

    smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,38 @@ $("#myModal").on("shown.bs.modal", function() {
 });
 
 $("#element").popover("show");
+
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+$("a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+    var diferencial = 0;
+
+    if (hash == "#contact") {
+      diferencial = 80;
+    }
+
+    if (hash == "#photo") {
+      diferencial = 50;
+    }
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - diferencial
+      },
+      200,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  } // End if
+});
